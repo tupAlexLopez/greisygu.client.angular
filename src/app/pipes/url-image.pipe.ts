176bf25981d650a01980620ef85c2e3de9f4dd58
tupAlexLopezz <tupalexlopez10@gmail.com>
2024-04-15 12:13:00 -0300

    add: Creacion de pipe para leer de manera optima el src de imagen

--- src/app/pipes/url-image.pipe.ts
+++ src/app/pipes/url-image.pipe.ts
@@ -1,12 +1,16 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { ProductResponse } from '../shared/interfaces/response.interface';
 
 @Pipe({
+  standalone:true,
   name: 'urlImage'
 })
 export class UrlImagePipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: ProductResponse ): string {
+    if(!value.img) return 'assets/no-image.png';
+
+    return value.img;
   }
 
 }
