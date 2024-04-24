4f76e139711066b9aa30000042ef845a53094d71
tupAlexLopezz <tupalexlopez10@gmail.com>
2024-04-24 20:04:21 -0300

    add: Agregue metodos de creacion, modificacion eliminacion al servicio de categorias

--- src/app/products/services/category.service.ts
+++ src/app/products/services/category.service.ts
@@ -1,16 +1,42 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, catchError, filter, map, of } from 'rxjs';
 import { CategoryResponse } from 'src/app/shared/interfaces/response.interface';
+import { environments } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
-
+  private base_url:string = environments.url + '/categories';
   constructor( private http:HttpClient ) { }
 
   getAll():Observable<CategoryResponse[]> {
-    return this.http.get<CategoryResponse[]>('http://localhost:8080/categories');
+    return this.http.get<CategoryResponse[]>( this.base_url );
+  }
+
+  save( categoryName:string ):Observable<boolean> {
+    const data = {
+      name: categoryName
+    }
+
+    return this.http.post( this.base_url, data )
+    .pipe( 
+      catchError( ()=> of( false ) ),
+      map( () => true ) 
+    );
+  }
+
+  update( category:CategoryResponse ):Observable<CategoryResponse> {
+    console.log( category );
+    return this.http.put<CategoryResponse>( this.base_url +`/${ category.id }`, category );
+  }
+
+  delete( idCategory:number ):Observable<boolean> {
+    return this.http.delete( this.base_url + `/${ idCategory }` )
+    .pipe( 
+      catchError( ()=> of( false ) ),
+      map( () => true )
+    );
   }
 }
