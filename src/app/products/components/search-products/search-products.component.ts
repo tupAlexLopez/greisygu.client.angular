28bb54d7a0218377839d41a2ff6ef072cca3a027
tupAlexLopezz <tupalexlopez10@gmail.com>
2024-04-26 13:51:39 -0300

    add: Cambio en vista HTML y componente de busqueda

--- src/app/products/components/search-products/search-products.component.ts
+++ src/app/products/components/search-products/search-products.component.ts
@@ -3,55 +3,34 @@ import { FormControl } from '@angular/forms';
 import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 
 import { ProductService } from '../../services/product.service';
-import { Subject, Subscription, debounceTime } from 'rxjs';
+import { Subject, Subscription, debounceTime, map } from 'rxjs';
 
 @Component({
   selector: 'product-search',
   templateUrl: './search-products.component.html',
   styleUrls: ['./search-products.component.css']
 })
-export class SearchProductsComponent implements OnInit, OnDestroy{
-  
-  @Output() searchBox = new EventEmitter<string>();
+export class SearchProductsComponent {
+  @Output() eventSearchValue = new EventEmitter<string>();
 
-  private debouncerSubscription?:Subscription;
-  private debounce:Subject<string> = new Subject();
-
-  public searchInput:FormControl = new FormControl();
   public descriptions:string[] = [];
+  public searchInput:FormControl = new FormControl();
 
   constructor(
     private productService:ProductService
   ){ }
 
-  ngOnInit(): void {
-    this.debouncerSubscription = this.debounce
-      .pipe(
-        debounceTime(400),
-      )
-      .subscribe(value => this.searchBox.emit(value));
-  }
-  ngOnDestroy(): void {
-    this.debouncerSubscription?.unsubscribe();
-  }
-
   searchProduct() {
     const value:string = this.searchInput.value || '';
     if(value === '') return;
-    //TODO: Arreglar aqui!
-    // this.productService.getAllByDescription( value )
-    //   .subscribe(prod => {
-    //     this.descriptions =prod.map( p => p.description )
-    //   });
+    
+    this.productService.getAllByDescription( value )
+    .subscribe( response => this.descriptions = response );
   }
 
   onSelectedOption( event:MatAutocompleteSelectedEvent ):void { 
-    this.searchBox.emit( event.option.value ); 
-  }
-
-  onKeyPress( value:string ):void {
-    this.debounce.next( value );
+    this.eventSearchValue.emit( event.option.value ); 
   }
   
-  onInputEnter( value:string ):void { this.searchBox.emit( value ); }
+  onInputEnter( value:string ):void { this.eventSearchValue.emit( value ); }
 }
