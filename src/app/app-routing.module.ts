e723892799211e1fa67a05469d33ba7ae591307a
tupAlexLopez <tupalexlopez10@gmail.com>
2024-05-02 15:58:27 -0300

    update:Cambio de directorio HomeComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './home/page/home.component';
 import { LayoutComponent } from './shared/layout/layout.component';
 
 const routes: Routes = [ 
