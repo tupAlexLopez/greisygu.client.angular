6eb71c91c99a62517c586f980e3a388f9db56d99
tupAlexLopez <tupalexlopez10@gmail.com>
2024-05-07 16:54:18 -0300

    add: Test al servicio de categorias

--- __test__/products/services/category.service.spec.ts
+++ __test__/products/services/category.service.spec.ts
@@ -1,16 +1,89 @@
 import { TestBed } from '@angular/core/testing';
 
 import { CategoryService } from '../../../src/app/products/services/category.service';
+import { HttpClientTestingModule, HttpTestingController, TestRequest } from '@angular/common/http/testing';
+import { CategoryResponse } from 'src/app/shared/interfaces/response.interface';
+import { of } from 'rxjs';
 
 describe('CategoryService', () => {
-  let service: CategoryService;
+  let categoryService: CategoryService;
+  let httpTestingController:HttpTestingController;
+
+  let mockCategories:CategoryResponse[];
+  let mockCategory:CategoryResponse;
+  let nameCategoryTest:string;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(CategoryService);
+    TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ],
+      providers: [ CategoryService ]
+    });
+
+    categoryService = TestBed.inject(CategoryService);
+    httpTestingController = TestBed.inject( HttpTestingController )
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  afterEach(()=> httpTestingController.verify());
+  beforeEach(() => {
+    
+  })
+
+  beforeAll(() => {
+    mockCategories = [ 
+      { id: 1, name: 'Prueba'}, 
+      { id: 2, name: 'Prueba 2'}, 
+      { id: 3, name: 'Prueba 3'}, 
+      { id: 4, name: 'Prueba 4'}, 
+      { id: 5, name: 'Prueba 5'}, 
+    ];
+
+    nameCategoryTest = 'Prueba test';
+    mockCategory   = { id: 5, name: 'Prueba de actualizacion' };
+  });
+
+  test('(1) Deberia inicializar el servicio.', () => {
+    expect( categoryService ).toBeTruthy(); 
+  });
+
+  test('(2) Deberia traer todas las categorias disponibles.', (done) => {
+    const spy = jest.spyOn(categoryService, 'getAll').mockReturnValue( of(mockCategories ));
+
+    categoryService.getAll().subscribe(response => {
+      expect( response[0].id ).toEqual( mockCategories[0].id );
+      expect( response[0].name ).toEqual( mockCategories[0].name );
+
+      expect( response ).toEqual(mockCategories);
+
+      done();
+    });
+
+    expect(spy).toHaveBeenCalled();
+  });
+
+  test('(3) Deberia guardar una categoria.', () => {
+    const spy = jest.spyOn(categoryService, 'save').mockReturnValue(of( true ));
+  
+    categoryService.save( nameCategoryTest ).subscribe(response => expect( response ).toBeTruthy());
+
+    expect(spy).toHaveBeenCalled();
+  });
+
+  test('(4) Deberia actualizar una categoria existente.', () => {
+    const spy = jest.spyOn(categoryService, 'update').mockReturnValue(of( mockCategory ));
+  
+    categoryService.update( mockCategory ).subscribe(response =>{
+      expect(response.id).toEqual( mockCategory.id );
+      expect(response.name).toEqual( mockCategory.name );
+    });
+
+    expect(spy).toHaveBeenCalled();
+  });
+
+  test('(5) Deberia eliminar una categoria.', () => {
+    const spy = jest.spyOn(categoryService, 'delete').mockReturnValue(of( true ));
+  
+    categoryService.delete( mockCategory.id ).subscribe(response => expect( response ).toBeTruthy());
+
+    expect(spy).toHaveBeenCalled();
   });
 });
