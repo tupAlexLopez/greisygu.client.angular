ec744754e54d52a0c2769381d9611873b9ae1596
alex <tupalexlopez10@gmail.com>
2024-05-08 17:51:55 -0300

    add: Testing servicio de validacion

--- __test__/products/services/validator.service.spec.ts
+++ __test__/products/services/validator.service.spec.ts
@@ -1,16 +1,71 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ValidatorService } from '../../../src/app/products/services/validator.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 describe('ValidatorService', () => {
   let service: ValidatorService;
+
+  let form:FormGroup;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(ValidatorService);
   });
 
-  it('should be created', () => {
+  beforeEach(()=> {
+    form = new FormGroup({
+      description: new FormControl('', Validators.required),
+      price: new FormControl('', Validators.pattern( service.pricePattern ))
+    });
+  });
+
+  test('Deberia inicializarse el servicio', () => {
     expect(service).toBeTruthy();
   });
+
+  test('Los campos del formulario deberian ser correctos.', ()=> {
+    const description:string = 'Producto';
+    const price:string = '123';
+    form.get('description')?.setValue( description );
+    form.get('price')?.setValue( price );
+
+  
+    const resultValidationDescriptionField:Boolean | null = service.isInvalidField( form, 'description' );
+    const resultValidationPriceField:Boolean | null = service.isInvalidField( form, 'price' );
+
+    expect( resultValidationDescriptionField ).toBeFalsy();
+    expect( resultValidationPriceField ).toBeFalsy();
+  })
+  
+  test('Los campos del formulario deberian ser incorrectos.', ()=> {
+    const description:string | null = null;
+    const price:string = '123asd';
+    form.get('description')?.setValue( description );
+    form.get('price')?.setValue( price );
+    form.markAllAsTouched();
+  
+    const resultValidationDescriptionField:Boolean | null = service.isInvalidField( form, 'description' );
+    const resultValidationPriceField:Boolean | null = service.isInvalidField( form, 'price' );
+
+    expect( resultValidationDescriptionField ).toBeTruthy();
+    expect( resultValidationPriceField ).toBeTruthy();
+  })
+  
+  test('Deberia indicarme cual fue el error en los campos.', ()=> {
+    const description:string | null = null;
+    const price:string = '123asd';
+
+    form.get('description')?.setValue( description );
+    form.get('price')?.setValue( price );
+    form.markAllAsTouched();
+  
+    const errorDescriptionField:string | null = service.getFieldError( form, 'description' );
+    const errorPriceField:string | null = service.getFieldError( form, 'price' );
+
+    expect( errorDescriptionField ).not.toBeNull();
+    expect( errorPriceField ).not.toBeNull();
+    expect( errorDescriptionField ).toBe('Campo obligatorio.');
+    expect( errorPriceField ).toBe('Debe cumplir expresion regular.');
+  })
 });
