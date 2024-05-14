8949258685d470a21f5b0c103805b0c7340e03f6
Alex Lopez <tupalexlopez10@gmail.com>
2024-05-14 17:51:29 -0300

    delete: Remove temporal test

--- __test__/pipes/string-boolean.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { StringBooleanPipe } from '../../src/app/pipes/string-boolean.pipe';
-
-describe('StringBooleanPipe', () => {
-  it('create an instance', () => {
-    const pipe = new StringBooleanPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- __test__/pipes/url-image.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { UrlImagePipe } from '../../src/app/pipes/url-image.pipe';
-
-describe('UrlImagePipe', () => {
-  it('create an instance', () => {
-    const pipe = new UrlImagePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
